eb85ebafdd2bfc3f5d2951ad74da79fe9814247d
Jesher Joshua <jesher.joshua@gmail.com>
2023-11-19 21:18:19 +0530

    add docstring

--- scripts/inference_tracking.py
+++ scripts/inference_tracking.py
@@ -1,4 +1,22 @@
 
+"""
+Author: Jesher Joshua M
+Email: jesherjoshua.m2021@vitstudent.ac.in
+
+Description:
+This script implements object tracking using YOLOv8 on video frames. It tracks objects, visualizes their paths,
+and generates fake locations based on the tracking results.
+
+Libraries Used:
+- OpenCV (cv2)
+- Ultralytics YOLO and NAS
+- Supervision
+- NumPy
+- SciPy
+- Collections
+
+Note: Make sure to install the required libraries using pip install [library_name].
+"""
 import cv2
 from ultralytics import YOLO
 from ultralytics import NAS
@@ -9,17 +27,30 @@ import robo_gen as rg
 from collections import defaultdict
 
 def mid_point(coord):
+    """
+    Calculate the mid-point of a bounding box given its coordinates.
+
+    Args:
+        coord (tuple): Coordinates in the format (x1, y1, x2, y2).
+
+    Returns:
+        tuple: Mid-point coordinates (x, y).
+    """
     x1, y1, x2, y2 = coord
-    x = (x1 +  x2)/ 2
-    y = (y1 + y2)/2
+    x = (x1 + x2) / 2
+    y = (y1 + y2) / 2
 
     return int(x), int(y)
 
 def xyxy_to_xywh(xyxy):
     """
-    Convert XYXY format (x,y top left and x,y bottom right) to XYWH format (x,y center point and width, height).
-    :param xyxy: [X1, Y1, X2, Y2]
-    :return: [X, Y, W, H]
+    Convert XYXY format (x, y top left and x, y bottom right) to XYWH format (x, y center point and width, height).
+
+    Args:
+        xyxy (list): List in the format [X1, Y1, X2, Y2].
+
+    Returns:
+        numpy.ndarray: Array in the format [X, Y, W, H].
     """
     if np.array(xyxy).ndim > 1 or len(xyxy) > 4:
         raise ValueError('xyxy format: [x1, y1, x2, y2]')
@@ -28,31 +59,51 @@ def xyxy_to_xywh(xyxy):
     w_temp = abs(xyxy[0] - xyxy[2])
     h_temp = abs(xyxy[1] - xyxy[3])
     return np.array([int(x_temp), int(y_temp), int(w_temp), int(h_temp)])
+
 def bottom_mid(coords):
-    x1,y1,x2,y2=coords
-    x=(x1+x2)//2
-    y=y2
-    return int(x),int(y)
+    """
+    Calculate the bottom mid-point of a bounding box given its coordinates.
 
-def distance(xy,xxyy):
-    return spatial.distance.euclidean(xy,xxyy)
+    Args:
+        coords (tuple): Coordinates in the format (x1, y1, x2, y2).
+
+    Returns:
+        tuple: Bottom mid-point coordinates (x, y).
+    """
+    x1, y1, x2, y2 = coords
+    x = (x1 + x2) // 2
+    y = y2
+    return int(x), int(y)
+
+def distance(xy, xxyy):
+    """
+    Calculate the Euclidean distance between two points.
+
+    Args:
+        xy (tuple): Coordinates of the first point.
+        xxyy (tuple): Coordinates of the second point.
+
+    Returns:
+        float: Euclidean distance between the two points.
+    """
+    return spatial.distance.euclidean(xy, xxyy)
+
 def main():
+    # Open video capture object
+    cap = cv2.VideoCapture("./data/vid_long.mp4")
+    
+    # Load YOLO model
+    model = YOLO('./models/yolov8n_nc4_trashcanext.pt')
 
-    cap = cv2.VideoCapture(
-        "./data/vid_1.mp4"
-    )
-    # input_fps=cap.get(cv2.CAP_PROP_FPS)
-    # w=int(cap.get(3))
-    # h=int(cap.get(4))
-    model = YOLO('./models/yolov8n_10e.pt')
-
-    # model=NAS("yolo_nas_s")
-    box_annotator = sv.BoxAnnotator(thickness=2, text_thickness=2, text_scale=1)
-# Store the track history
+    # Store the track history
     track_history = defaultdict(lambda: [])
-    n=0
+    n = 0
+    counter = 0
+
     # Loop through the video frames
     while cap.isOpened():
+        counter += 1
+
         # Read a frame from the video
         success, frame = cap.read()
 
@@ -65,31 +116,36 @@ def main():
             try:
                 track_ids = results[0].boxes.id.int().cpu().tolist()
             except:
-                track_ids=[]
+                track_ids = []
 
             # Visualize the results on the frame
             annotated_frame = results[0].plot()
 
             # Plot the tracks
             for box, track_id in zip(boxes, track_ids):
-                n= track_id if track_id>n else n
+                n = track_id if track_id > n else n
                 x, y, w, h = box
                 track = track_history[track_id]
                 track.append((float(x), float(y)))  # x, y center point
+
                 if len(track) > 30:  # retain 90 tracks for 90 frames
                     track.pop(0)
 
                 # Draw the tracking lines
                 points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
-                cv2.polylines(annotated_frame, [points], isClosed=False, color=(0,0,255), thickness=3)
+                cv2.polylines(annotated_frame, [points], isClosed=False, color=(0, 0, 255), thickness=3)
 
             # Display the annotated frame
             cv2.imshow("YOLOv8 Tracking", annotated_frame)
 
             # Break the loop if 'q' is pressed
+            if counter > 100:
+                print(f'count: {n}')
+                rg.gen_fake_loc(n, 'count')
+                counter = 0
+                n = 0
+                model = YOLO('./models/yolov8n_nc4_trashcanext.pt')
 
-            if n!=0:
-                rg.gen_fake_loc(n,'count')
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
         else:
